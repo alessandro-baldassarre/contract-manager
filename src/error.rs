d4e3f4f99a4498741d79087735728817476172f0
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-20 19:55:45 +0100

    Add EncodeError

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 use cosmwasm_std::StdError;
 use cw_controllers::AdminError;
 use cw_utils::ParseReplyError;
+use prost::EncodeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -11,6 +12,8 @@ pub enum ContractError {
     Reply(#[from] ParseReplyError),
     #[error("{0}")]
     Admin(#[from] AdminError),
+    #[error("{0}")]
+    Prost(#[from] EncodeError),
     #[error("Unauthorized")]
     Unauthorized {},
     // Add any other custom errors you like here.
