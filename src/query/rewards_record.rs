b1c641ab304855269f129a0a7f566072383e15e6
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-24 12:45:40 +0100

    Update query rewards

--- src/query/rewards_record.rs
+++ src/query/rewards_record.rs
@@ -2,18 +2,19 @@ use archway_sdk::custom::{
     query::ArchwayQuery,
     types::{PageRequest, RewardsRecordsResponse},
 };
-use cosmwasm_std::{Deps, StdResult};
+use cosmwasm_std::{Deps, Env, StdResult};
 
 pub fn query(
     deps: Deps<ArchwayQuery>,
-    rewards_address: String,
+    env: Env,
     pagination: Option<PageRequest>,
 ) -> StdResult<RewardsRecordsResponse> {
     let request = match pagination {
         Some(page_request) => {
-            ArchwayQuery::rewards_records_with_pagination(rewards_address, page_request).into()
+            ArchwayQuery::rewards_records_with_pagination(&env.contract.address, page_request)
+                .into()
         }
-        None => ArchwayQuery::rewards_records(rewards_address).into(),
+        None => ArchwayQuery::rewards_records(&env.contract.address).into(),
     };
     deps.querier.query(&request)
 }
