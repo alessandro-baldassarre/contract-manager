1f7443690ca494c6b08cd42be2d4f6eca908ca09
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-24 15:13:28 +0100

    Refactor:format code for clippy warning

--- src/query/contracts_list.rs
+++ src/query/contracts_list.rs
@@ -4,8 +4,9 @@ use cosmwasm_std::{Deps, Order, StdResult};
 use crate::state::{Contract, CONTRACTS_LIST};
 
 pub fn query(deps: Deps<ArchwayQuery>) -> StdResult<Vec<(String, Contract)>> {
-    let contracts: StdResult<Vec<_>> = CONTRACTS_LIST
+    let contracts_res: StdResult<Vec<_>> = CONTRACTS_LIST
         .range(deps.storage, None, None, Order::Descending)
         .collect();
-    Ok(contracts?)
+    let contracts = contracts_res?;
+    Ok(contracts)
 }
