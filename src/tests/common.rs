e717aa97ef4b98287eac80d36afee05f36900e68
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-26 00:38:04 +0100

    Feat:add unit-test

--- src/tests/common.rs
+++ src/tests/common.rs
@@ -1,10 +1,15 @@
 use archway_sdk::custom::query::ArchwayQuery;
 use cosmwasm_std::{
+    from_binary,
     testing::{mock_env, mock_info},
-    DepsMut,
+    Deps, DepsMut, Response,
 };
+use serde::de::DeserializeOwned;
 
-use crate::{contract::instantiate, msg::InstantiateMsg};
+use crate::{
+    contract::{execute, instantiate, query},
+    msg::{ExecuteMsg, InstantiateMsg, QueryMsg},
+};
 
 pub const INIT_OWNER: &str = "balda";
 
@@ -14,4 +19,14 @@ pub fn mock_instantiate(deps: DepsMut<ArchwayQuery>) {
     };
     let info = mock_info("creator", &[]);
     instantiate(deps, mock_env(), info, msg).unwrap();
+}
+
+pub fn mock_execute(deps: DepsMut<ArchwayQuery>, msg: ExecuteMsg) -> Response {
+    let env = mock_env();
+    let info = mock_info("balda", &[]);
+    execute(deps, env, info, msg).unwrap()
+}
+
+pub fn mock_query<T: DeserializeOwned>(deps: Deps<ArchwayQuery>, msg: QueryMsg) -> T {
+    from_binary(&query(deps, mock_env(), msg).unwrap()).unwrap()
 }
--- src/tests/execute.rs
+++ src/tests/execute.rs
@@ -1,6 +1,8 @@
-use archway_sdk::test::mock_deps_archway;
+use archway_sdk::types::archwayrewardsv1beta1::MsgSetContractMetadata;
+use archway_test::mock_deps_archway;
+use cosmwasm_std::{testing::mock_env, Binary, CosmosMsg, SubMsg, WasmMsg};
 
-use crate::contract::ADMIN;
+use crate::{contract::ADMIN, msg::ExecuteMsg, tests::common::mock_execute};
 
 use super::common::{mock_instantiate, INIT_OWNER};
 
@@ -11,4 +13,56 @@ fn instantiate_contract() {
 
     let res = ADMIN.query_admin(deps.as_ref()).unwrap();
     assert_eq!(Some(INIT_OWNER.into()), res.admin);
+
+    let msg = ExecuteMsg::InstantiateContract {
+        code_id: 1,
+        instantiate_msg: Binary(vec![]),
+        label: "test".to_string(),
+    };
+    let res = mock_execute(deps.as_mut(), msg);
+
+    let instantiate_msg = WasmMsg::Instantiate {
+        admin: Some(mock_env().contract.address.to_string()),
+        code_id: 1,
+        msg: Binary(vec![]),
+        funds: vec![],
+        label: "test".to_string(),
+    };
+    assert_eq!(
+        vec![SubMsg::reply_on_success(instantiate_msg, 1)],
+        res.messages
+    );
+}
+
+#[test]
+fn set_contract_metadata() {
+    let mut deps = mock_deps_archway();
+    mock_instantiate(deps.as_mut());
+
+    let res = ADMIN.query_admin(deps.as_ref()).unwrap();
+    assert_eq!(Some(INIT_OWNER.into()), res.admin);
+
+    let msg = ExecuteMsg::InstantiateContract {
+        code_id: 1,
+        instantiate_msg: Binary(vec![]),
+        label: "test".to_string(),
+    };
+    mock_execute(deps.as_mut(), msg);
+
+    let contract_address = "test_contract".to_string();
+    let msg = ExecuteMsg::SetContractMetadata {
+        contract_address: contract_address.clone(),
+        rewards_address: Some(mock_env().contract.address.to_string()),
+    };
+
+    let res_set_metadata = mock_execute(deps.as_mut(), msg);
+
+    let msg: CosmosMsg = MsgSetContractMetadata::new(
+        &mock_env().contract.address,
+        contract_address,
+        &mock_env().contract.address,
+        &mock_env().contract.address,
+    )
+    .into();
+    assert_eq!(vec![SubMsg::new(msg)], res_set_metadata.messages)
 }
